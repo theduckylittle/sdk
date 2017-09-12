fef9c77969dff626e92f67e048519c6d41aebbc6
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-09-12 12:19:44 -0500

    Added Bounding Box support to geojson type sources
    1. Refactored the code to override the Vector Source's
       loader function. This allows SDK to interpret how features
       should be added to the source inside of the OpenLayers update
       cycle (specifically needed for BBOXes).
    2. Added test to prove the URL will fetch with a bbox.

--- __tests__/components/map-geojson-source.test.js
+++ __tests__/components/map-geojson-source.test.js
@@ -121,6 +121,15 @@ describe('tests for the geojson-type map sources', () => {
     testGeojsonData(done, './test.geojson', 2);
   });
 
+  it('adds a geojson feature collection with a BBOX', (done) => {
+    // mock up the url to call
+    nock('http://example.com')
+      .get('/base/bbox.geojson?BBOX=-978393.9620502561,-978393.9620502568,978393.9620502561,978393.9620502554')
+      .reply(200, JSON.stringify(feature_collection));
+
+    testGeojsonData(done, '/bbox.geojson?BBOX={bbox-epsg-3857}', 2);
+  });
+
   it('tries to fetch a geojson file that does not exist', (done) => {
     testGeojsonData(done, 'http://example.com/no-where.geojson', 0);
   });
